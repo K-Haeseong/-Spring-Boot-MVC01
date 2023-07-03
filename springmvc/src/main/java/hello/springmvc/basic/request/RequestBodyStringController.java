1622cd927e275c9d79d43f476ef007de8e108fec
Aiden <kim_aiden@naver.com>
2023-07-03 21:56:59 +0900

    HTTP 요청 메시지 - JSON 응답 - 정적 리소스, 뷰 템플릿 HTTP 응답 - HTTP API, 메시지 바디에 직접입력 HTTP 메시지 컨버터 요청 매핑 핸들러 어뎁터 구조

--- springmvc/src/main/java/hello/springmvc/basic/request/RequestBodyStringController.java
+++ springmvc/src/main/java/hello/springmvc/basic/request/RequestBodyStringController.java
@@ -13,6 +13,7 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 import java.io.InputStream;
+import java.io.OutputStream;
 import java.io.Writer;
 import java.nio.charset.StandardCharsets;
 
